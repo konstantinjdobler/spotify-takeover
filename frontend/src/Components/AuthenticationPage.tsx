f261386e30f2579c918d0c03a17df188aa78c6b1
konstantinjdobler <konstantin.j.dobler@gmail.com>
2020-03-13 16:09:19 -0700

    Update AuthenticationPage.tsx

--- frontend/src/Components/AuthenticationPage.tsx
+++ frontend/src/Components/AuthenticationPage.tsx
@@ -6,7 +6,7 @@ export default function AuthenticationLink(props: { authenticationLink: string }
     <Paper style={{ maxWidth: "600px", margin: "auto" }}>
       <CardContent>
         <Typography variant="h5" color="textPrimary">
-          Oh no, you are not logged in or signed up!
+          Oh no, you are not logged in or signed up! If you did not already request a signup link from me, please do so before authenticating with Spotify!
         </Typography>
         <Typography variant="body1" color="textSecondary">
           Don't worry, it's really easy. Click on the button below and authenticate with Spotify - you're ready to go!
